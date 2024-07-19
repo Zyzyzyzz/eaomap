ea03b16f26619ad5613cfecc781080953daea4ca
Zyzyzyzz <sulerplexh123@gmail.com>
2024-07-19 20:54:19 +0500

    Add files via upload

---
+++ Son.js
@@ -0,0 +1,7 @@
+console.log("Loading...");
+(function(){
+	let e = document.createElement(`script`);
+	e.src = `https://endlessnightnlr.github.io/MLPP/MLPP_Minimap.user.js`;
+	//e.src = `https://Autumn-Blaze.github.io/MLPP_Minimap.user.js`;
+	document.body.appendChild(e);
+})();
